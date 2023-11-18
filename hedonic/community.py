d63c06baf824f08b507b93bb99cec453bc575854
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2023-11-18 03:28:08 +0100

    plot a figure of heatmaps of acc for each method

--- hedonic/community.py
+++ hedonic/community.py
@@ -1,5 +1,5 @@
 import igraph as ig
 
-def community_hedonic(g):
+def community_hedonic(g, resolution=1):
     membership = [0] * g.vcount()
     return ig.clustering.VertexClustering(g, membership)
--- scripts/main.py
+++ scripts/main.py
@@ -1,37 +1,37 @@
 import igraph as ig
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 from networkx.generators.community import stochastic_block_model as SBM
 from hedonic import community_hedonic
 
 #################################################
 
-# Parameters
-community_size = 500 # Define block sizes
-p_in = .1
-multiplier = .25
-
-#################################################
+def networkx_to_igraph(g):
+    return ig.Graph(g.number_of_nodes(), g.edges())
 
 def probs_matrix(n_communities, p, q):
-    probs = np.full((n_communities, n_communities), q)
-    np.fill_diagonal(probs, p)
-    return probs
+    probs = np.full((n_communities, n_communities), q) # fill with q
+    np.fill_diagonal(probs, p) # fill diagonal with p
+    return probs # return probability matrix
 
-def ppm_igraph(community_size, p_in, multiplier):
-    # Define edge probabilities between blocks
-    block_sizes = [community_size] * 2
-    p_out = p_in * multiplier
-    p = probs_matrix(len(block_sizes), p_in, p_out)
-    h = SBM(block_sizes, p)
-    g = ig.Graph.TupleList(h.edges()) # convert to igraph
-    return g
+def ppm_igraph(n_communities, community_size, p_in, multiplier):
+    block_sizes = np.full(n_communities, community_size) # all blocks are same size
+    p_out = p_in * multiplier # probability of edge between communities
+    p = probs_matrix(n_communities, p_in, p_out) # probability matrix
+    h = SBM(block_sizes, p) # generate networkx graph
+    g = networkx_to_igraph(h) # convert to igraph
+    return g # return igraph graph
 
-def cap_n_communities(g, partition, max_n_comm=2):
+def cap_n_communities(g, partition, max_n_comm):
     new_partition = None
     if len(partition.sizes()) > max_n_comm:
         new_membership = [m if (
             m < max_n_comm
-        ) else (max_n_comm - 1) for m in partition.membership]
+        ) else (
+            max_n_comm - 1
+        ) for m in partition.membership]
         new_partition = ig.clustering.VertexClustering(g, new_membership)
     return new_partition if new_partition else partition
 
@@ -46,23 +46,85 @@ def accuracy(partition, ground_truth):
     acc = n_correct / partition.n ** 2
     return (acc - .5) / .5
 
+def generate_probability_array(length):
+    powers = np.arange(length-1, -1, -1)
+    result = 1 / np.power(2, powers)
+    return result
+
 #################################################
 
-g = ppm_igraph(community_size, p_in, multiplier)
+def run_experiment(
+        length_of_probability_array = 3, # Define length of probability array
+        community_size = 100, # Define block sizes (all blocks are same size)
+        number_of_communities = 2, # Define number of communities
+    ):
+    results = []
+    ps_in = generate_probability_array(length_of_probability_array) # Define probability of edge within community
+    multipliers = generate_probability_array(length_of_probability_array) # `multiplier = p_out / p_in` and p_out is the probability of edge between communities
+    gt_membership = np.concatenate((np.zeros(community_size), np.ones(community_size)))
+    ground_truth = ig.clustering.VertexClustering(g, gt_membership)
+    for p_in in ps_in:
+        for mult in multipliers:
+            g = ppm_igraph(number_of_communities, community_size, p_in, mult)
+            # Get the communities
+            comms_ml = g.community_multilevel(resolution=g.density())
+            comms_ld = g.community_leiden(resolution=g.density())
+            comms_hd = community_hedonic(g, resolution=g.density())
 
-# Get the communities
-comms_ml = g.community_multilevel()
-comms_ld = g.community_leiden(resolution=g.density())
-comms_hd = community_hedonic(g)
+            cap_comms_ml = cap_n_communities(g, comms_ml, number_of_communities)
+            cap_comms_ld = cap_n_communities(g, comms_ld, number_of_communities)
+            acc = accuracy(method['Partition'], ground_truth)
+            
+            # Define the methods
+            methods = [
+                {
+                    'Method': 'community multilevel',
+                    'Partition': cap_comms_ml
+                },
+                {
+                    'Method': 'community leiden',
+                    'Partition': cap_comms_ld
+                },
+                {
+                    'Method': 'community hedonic',
+                    'Partition': comms_hd
+                }
+            ]
 
-cap_comms_ml = cap_n_communities(g, comms_ml)
-cap_comms_ld = cap_n_communities(g, comms_ld)
+            # Calculate accuracy for each method
+            for method in methods:
+                results.append({
+                    'Method': method['Method'],
+                    'p_in': p_in,
+                    'p_out': p_in * mult,
+                    'Multiplier': mult,
+                    'Accuracy': accuracy(method['Partition'], ground_truth)
+                })
 
-gt_membership = np.concatenate(
-    (np.zeros(community_size), np.ones(community_size)))
-ground_truth = ig.clustering.VertexClustering(g, gt_membership)
+    # Create a pandas dataframe
+    df = pd.DataFrame(results)
 
-# Print the accuracy of the methods
-print('community multilevel acc: ', accuracy(cap_comms_ml, ground_truth))
-print('community leiden acc: ', accuracy(cap_comms_ld, ground_truth))
-print('community hedonic acc: ', accuracy(comms_hd, ground_truth))
+    return df
+
+#################################################
+
+def plot_heatmaps(df):
+    methods = df["Method"].unique()
+    fig, axes = plt.subplots(1, len(methods), figsize=(15, 5))
+    for i, method in enumerate(methods):
+        method_df = df[df["Method"] == method]
+        method_data = method_df.pivot_table(index="p_in", columns="Multiplier", values="Accuracy")
+        
+        ax = sns.heatmap(method_data, ax=axes[i], cmap="Greens", vmin=0, vmax=1) # , annot=True, fmt=".2f",
+        ax.set_title(method)
+        ax.set_xlabel("multiplier")
+        ax.set_ylabel("p_in")
+    
+    return fig, axes
+
+#################################################
+
+df = run_experiment(10)
+fig, axes = plot_heatmaps(df)
+plt.tight_layout()
+plt.show()
