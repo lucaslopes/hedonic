13400e3cc1fb6d8f0620426eb7ff7dc4c5991142
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2025-02-19 05:47:38 -0300

    Boolean arg in for do not generate commands for already completed communities

--- scripts/generate_commands.py
+++ scripts/generate_commands.py
@@ -1,10 +1,12 @@
 import os
 import re
+import argparse
 from tqdm import tqdm
 from utils import get_all_subpaths
 
-# Function to extract sorting keys from the file name
+
 def extract_sorting_keys(filename):
+    """Extract sorting keys from the file name"""
     match = re.search(r'network_(\d+)\.pkl', filename)
     network_index = int(match.group(1)) if match else float('inf')
     
@@ -19,42 +21,61 @@ def extract_sorting_keys(filename):
     
     return (network_index, n_communities, p_in, difficulty)
 
-# Sort the list of files based on the extracted keys
+
 def sort_files(file_list):
+    """Sort the list of files based on the extracted keys"""
     return sorted(file_list, key=extract_sorting_keys)
 
-# Configuration
-# suffix = 0
-main_session = 'HedonicSession'  # f"Session_{suffix}"
-python_script = "scripts/exp.py"
-networks_dir = "/Users/lucas/Databases/Hedonic/PHYSA/Synthetic_Networks/V1020/networks"
-env_activation = "source $(conda info --base)/etc/profile.d/conda.sh && conda activate hedonic"
 
-# Find all .pkl files
-pkl_files = get_all_subpaths(networks_dir, endswith=".pkl")
+def generate_command(file, main_session, python_script, env_activation):
+    """Generate a valid window name (last 4 subdirectories) and create the correct command format"""
+    window_name = "/".join(file[:-4].split("/")[-4:])
+    window_name = f'"{window_name}"'  # Ensure it's quoted properly
 
-# Sort the list of files
-sorted_commands = sort_files(pkl_files)
+    command = (
+        f"tmux new-window -t {main_session} -n {window_name} "
+        f"\"{env_activation} && "
+        f"exec python {python_script} \\\"{file}\\\"'\";\n"
+    )
 
-# Write commands to file
-community_count = 0
-commands_file = networks_dir.replace("/networks", "/commands.txt")
-with open(commands_file, "w") as f:
-    for file in tqdm(sorted_commands[:1000], desc="Generating commands"):
-        # Generate a valid window name (last 4 subdirectories)
-        window_name = "/".join(file[:-4].split("/")[-4:])
-        window_name = f'"{window_name}"'  # Ensure it's quoted properly
+    return command
 
-        # Create the correct command format
-        command = (
-            f"tmux new-window -t {main_session} -n {window_name} "
-            f"\"bash -i -c 'source $(conda info --base)/etc/profile.d/conda.sh && conda activate hedonic && "
-            f"exec python {python_script} \\\"{file}\\\"'\";\n"
-        )
 
-        # if not os.path.exists(file.replace('.pkl', '.completed'))
-        if ('network_000.pkl' in file):
-            f.write(command)
-            community_count += 1
+def write_commands(sorted_commands, main_session, python_script, env_activation, commands_file, filter_completed=False):
+    community_count = 0
+    with open(commands_file, "w") as f:
+        for file in tqdm(sorted_commands, desc="Generating commands"):
+            command = generate_command(file, main_session, python_script, env_activation)
+            if not filter_completed or not os.path.exists(file.replace('.pkl', '.completed')):
+                f.write(command)
+                community_count += 1
+    return community_count
 
-print(f"Generated {community_count} commands in {commands_file}")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filter-completed", action="store_true", help="Do not generate commands for already completed communities")
+    args = parser.parse_args()
+
+    # Configuration
+    suffix = 0
+    main_session = f"HedonicSession_{suffix}"
+    python_script = "scripts/exp.py"
+    networks_dir = "/Users/lucas/Databases/Hedonic/PHYSA/Synthetic_Networks/V1020/networks"
+    env_activation = "source $(conda info --base)/etc/profile.d/conda.sh && conda activate hedonic"
+
+    # Find all .pkl files
+    pkl_files = get_all_subpaths(networks_dir, endswith=".pkl")
+
+    # Sort the list of files
+    sorted_commands = sort_files(pkl_files)
+
+    # Write commands to file
+    commands_file = networks_dir.replace("/networks", "/commands.txt")
+    community_count = write_commands(sorted_commands, main_session, python_script, env_activation, commands_file, filter_completed=args.filter_completed)
+
+
+    print(f"Generated {community_count} commands in {commands_file}")
+
+
+if __name__ == "__main__":
+    main()
