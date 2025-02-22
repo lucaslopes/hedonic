84c611bccf47a821215ebcfaa1df156e614706e5
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2025-02-22 18:06:22 -0300

    Pass path strings to each step instead of always searching for the path of each file

--- scripts/data_reader.py
+++ scripts/data_reader.py
@@ -8,7 +8,7 @@ from concurrent.futures import ProcessPoolExecutor
 import pandas as pd
 from tqdm import tqdm
 from utils import sort_files
-WORKERS = 4
+WORKERS = 16
 
 
 def collect_paths(leaf_folder: str, extension: str = ".json") -> list[str]:
@@ -48,7 +48,7 @@ def get_subroots(root: str, max_depth: int = 1) -> list[str]:
     """
     subroots = []
     # Walk the tree and measure depth relative to root.
-    for dirpath, dirs, _ in tqdm(os.walk(root), desc="Walking directory tree for subroots"):
+    for dirpath, dirs, _ in tqdm(os.walk(root), desc="Walking directory tree for subroots", leave=False):
         # Calculate relative depth (number of separators after the root)
         rel_depth = dirpath[len(root):].count(os.sep)
         if rel_depth == max_depth:
@@ -96,9 +96,10 @@ def get_paths_sorted(folder_path: str, extension: str = ".json") -> list[str]:
         results = list(tqdm(
             executor.map(collect_paths_helper, args_list),
             total=len(leaf_dirs),
-            desc=f"Collecting {extension} paths"
+            desc=f"Collecting {extension} paths",
         ))
     file_paths = [file for sublist in results for file in sublist]
+    print(f"Found {len(file_paths)} {extension} files to be sorted.")
     file_paths = sort_files(file_paths)
     return file_paths
 
@@ -151,7 +152,7 @@ def dump_all_json_paths(
         list_of_json_lists: list[list[str]],
         verbose: bool = False,
         output_folder: str = "json_paths"
-    ) -> bool:
+    ) -> list[str]:
     """
     Dumps each list of file paths from list_of_json_lists into a separate text file.
     """
@@ -175,10 +176,11 @@ def dump_all_json_paths(
             total=len(tasks),
             desc="Dumping JSON paths"
         ))
-    return True
+    output_files = [task[1] for task in tasks]
+    return output_files
 
 
-def dump_csv(txt_path: str, ignore_partition_key=True, output_folder: str = "csv_results") -> bool:
+def dump_csv(txt_path: str, ignore_partition_key=True, output_folder: str = "csv_results") -> str:
     """
     Reads a text file of file paths and dumps the JSON data into a CSV file.
     """
@@ -196,7 +198,7 @@ def dump_csv(txt_path: str, ignore_partition_key=True, output_folder: str = "csv
     csv_path = txt_path.replace('/json_paths/', f'/{output_folder}/').replace('.txt', '.csv.gzip')
     os.makedirs(os.path.dirname(csv_path), exist_ok=True)
     df.to_csv(csv_path, index=False, compression="gzip")
-    return True
+    return csv_path
 
 
 def dump_csv_helper(args: tuple[str, str]) -> bool:
@@ -207,19 +209,22 @@ def dump_csv_helper(args: tuple[str, str]) -> bool:
     return dump_csv(txt_path, output_folder=output_folder)
 
 
-def dump_all_csv(json_path: str, output_folder: str = "csv_results") -> bool:
+def dump_all_csv(json_path: str | list[str], output_folder: str = "csv_results") -> bool:
     """
     Dumps all CSV files found in a directory tree.
     """
-    sorted_txt_paths = get_paths_sorted(json_path, extension=".txt")
+    if isinstance(json_path, str):
+        sorted_txt_paths = get_paths_sorted(json_path, extension=".txt")
+    else:
+        sorted_txt_paths = sort_files(json_path)
     args_list = [(fp, output_folder) for fp in sorted_txt_paths]
     with ProcessPoolExecutor(max_workers=WORKERS) as executor:
-        list(tqdm(
+        csv_paths = list(tqdm(
             executor.map(dump_csv_helper, args_list),
             total=len(sorted_txt_paths),
             desc="Dumping CSV files"
         ))
-    return True
+    return csv_paths
 
 
 def read_csv_helper(fp: str) -> pd.DataFrame:
@@ -229,11 +234,14 @@ def read_csv_helper(fp: str) -> pd.DataFrame:
     return pd.read_csv(fp, compression="gzip")
 
 
-def get_combined_dataframe(csv_path: str) -> pd.DataFrame:
+def get_combined_dataframe(csv_path: str | list[str]) -> pd.DataFrame:
     """
     Gets a combined DataFrame from a list of CSV files.
     """
-    sorted_csv_paths = get_paths_sorted(csv_path, extension=".csv.gzip")
+    if isinstance(csv_path, str):
+        sorted_csv_paths = get_paths_sorted(csv_path, extension=".csv.gzip")
+    else:
+        sorted_csv_paths = sort_files(csv_path)
     with ProcessPoolExecutor(max_workers=WORKERS) as executor:
         df_list = list(tqdm(
             executor.map(read_csv_helper, sorted_csv_paths),
@@ -250,13 +258,11 @@ def load_experiment_data(results_folder: str):
     """
     sorted_json_paths = get_paths_sorted(results_folder)
     split_paths = split_json_paths(sorted_json_paths)
-    dump_all_json_paths(split_paths)
+    txt_paths = dump_all_json_paths(split_paths)
     time.sleep(2)
-    json_path = results_folder.replace('/resultados/', '/json_paths/')
-    dump_all_csv(json_path)
+    csv_paths = dump_all_csv(txt_paths)
     time.sleep(2)
-    csv_path = results_folder.replace('/resultados/', '/csv_results/')
-    df = get_combined_dataframe(csv_path)
+    df = get_combined_dataframe(csv_paths)
     return df
 
 
